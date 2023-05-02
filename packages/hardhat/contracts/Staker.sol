9ab8c792aeff509b140380f4d775afdf14f3eb47
Роман Савощик <tweakice@gmail.com>
2023-05-02 15:56:52 +0600

    add stake function

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -4,9 +4,15 @@ pragma solidity 0.8.4;  //Do not change the solidity version as it negativly imp
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
+error InvalidStakeAmount(string message);
+
 contract Staker {
 
+  event Stake(address indexed from, uint256 amount);
+
   ExampleExternalContract public exampleExternalContract;
+  uint256 public constant threshold = 1 ether;
+  mapping ( address => uint256 ) public balances;
 
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
@@ -14,7 +20,19 @@ contract Staker {
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() payable public {
+    if (msg.value == 0) {
+      revert InvalidStakeAmount({ message: "More than 0 wei needed!" });
+    }
 
+    if (balances[msg.sender] == 0) {
+      balances[msg.sender] = msg.value;
+    } else {
+      balances[msg.sender] += msg.value;
+    }
+
+    emit Stake(msg.sender, msg.value);
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
