700eb0d80e3b41519e46169375a9e77b917c4856
Роман Савощик <tweakice@gmail.com>
2023-05-02 16:52:39 +0600

    add withdraw functioanality

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,6 +5,10 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 error InvalidStakeAmount(string message);
+error DeadlineNotMet();
+error NotOpenToWithdraw();
+error InsufficientBalance();
+error FailedWithdrawTx();
 
 contract Staker {
 
@@ -12,6 +16,8 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
   uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 30 seconds;
+  bool public openToWithdraw;
   mapping ( address => uint256 ) public balances;
 
   constructor(address exampleExternalContractAddress) {
@@ -36,13 +42,50 @@ contract Staker {
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+  function execute() public {
+    if (block.timestamp < deadline) {
+      revert DeadlineNotMet();
+    }
 
+    if (address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+    } else {
+      openToWithdraw = true;
+    }
+    
+  }
 
   // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
+  function withdraw() public {
+    if (!openToWithdraw) {
+      revert NotOpenToWithdraw();
+    }
 
+    if (balances[msg.sender] == 0) {
+      revert InsufficientBalance();
+    }
+
+    // Call returns a boolean value indicating success or failure.
+    (bool sent, bytes memory data) = msg.sender.call
+    {
+      value: balances[msg.sender]
+    }("");
+
+    if (!sent) {
+      revert FailedWithdrawTx();
+    }
+
+    // Clear balances after withdraw
+    delete balances[msg.sender];
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 
   // Add the `receive()` special function that receives eth and calls stake()
 
